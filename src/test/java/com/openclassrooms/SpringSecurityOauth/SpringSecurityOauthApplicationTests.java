fc8c5ca4b22eb6c4809a374f1debb7d3f86dea50
sarra.naifar <naifarsara@gmail.com>
2020-05-10 22:55:41 +0200

    first commit

---
+++ src/test/java/com/openclassrooms/SpringSecurityOauth/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.SpringSecurityOauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
